335de8e8ed4652c550d1ce876d1caef5c8bdad81
adam <adam@20776cb8-8444-0410-85a6-d943369e813c>
2008-03-26 23:35:00 +0000

    Made a few tweaks to the blog page -- added links, etc.; temporarily removed the about page; set the background of the work page to white for smoother rendering.
    git-svn-id: svn+ssh://clearelement.co/var/svn/aad/trunk@18 20776cb8-8444-0410-85a6-d943369e813c

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
 
   map.home '', :controller => 'site', :action => 'home'
-  map.about '/about', :controller => 'site', :action => 'about'
+  #map.about '/about', :controller => 'site', :action => 'about'
   map.contact '/contact', :controller => 'site', :action => 'contact'
   map.blog '/blog', :controller => 'site', :action => 'blog'
   map.work '/work/:id', :controller => 'site', :action => 'work'
