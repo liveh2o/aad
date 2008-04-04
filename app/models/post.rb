bec55a555c5b91dce7a6ef529f7076fdd15d65f8
adam <adam@20776cb8-8444-0410-85a6-d943369e813c>
2008-04-04 03:17:06 +0000

    For some reason, posted_on doesn't properly order; now ordering by id
    git-svn-id: svn+ssh://clearelement.co/var/svn/aad/trunk@29 20776cb8-8444-0410-85a6-d943369e813c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   has_and_belongs_to_many :tags
-  has_many :comments, :order => "posted_on DESC"
+  has_many :comments, :order => "id DESC"
   has_one :image, :class_name => 'BlogImage'
   
   def url
